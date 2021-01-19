c72a6dc76ffdf86b4030e13aeb9c01350ea80d9c
sirayusan <pikachu.endu@gmail.com>
2021-01-20 02:31:27 +0900

    FollowControllerの不要部削除

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -9,30 +9,10 @@ use Auth;
 
 class FollowController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
     public function post_index()
     {
         $user = User::find(Auth::id());
         return view('follow_post',compact('user'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
     }
 
     /**
@@ -55,40 +35,6 @@ class FollowController extends Controller
          $follow->save();
          return redirect()->back();
      }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
 
     /**
      * Remove the specified resource from storage.
