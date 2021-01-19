ff70fdea2fc549d4aa90b173f587799568c0a118
sirayusan <pikachu.endu@gmail.com>
2021-01-20 02:57:34 +0900

    シードファイルの命名変更

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             PostSeeder::class,
             RepliesSeeder::class,
             FolllowSeeder::class,
+            TagsSeeder::class,
         ]);
     }
 }
