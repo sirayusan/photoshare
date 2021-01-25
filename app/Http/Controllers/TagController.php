c3fb35dbe7b376d641950b20786d3f4709737d5f
sirayusan <pikachu.endu@gmail.com>
2021-01-25 19:41:48 +0900

    タグ登録削除処理訂正

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Tag;
+use App\Models\Post;
 
 class TagController extends Controller
 {
@@ -15,16 +16,23 @@ class TagController extends Controller
     */
     public function index(Request $request)
     {
-        $substitute_tags = explode(',',str_replace('，',',',$request->tags));
-        $tags = Tag::wherein('tag',$substitute_tags)->get();
-        foreach ($tags as $tag)
+        if (isset($request->tags))
         {
-            foreach ($tag->posts as $post)
+            $substitute_tags = array_unique(explode(',',str_replace('，',',',$request->tags)));
+            $tags = Tag::wherein('tag',$substitute_tags)->get();
+            foreach ($tags as $tag)
             {
-                $posts[] = $post;
+                foreach ($tag->posts as $post)
+                {
+                    $posts[] = $post;
+                }
             }
+            $posts = array_unique($posts);
+            return view('tag_search',compact('posts'));
+        }else{
+            $posts = Post::all();
+            $comment = "入力がなかったため全数表示しています。";
+            return view('tag_search',compact('posts','comment'));
         }
-        $posts = array_unique($posts);
-        return view('tag_search',compact('posts'));
     }
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -15,4 +15,9 @@ class Tag extends Model
     {
        return $this->hasMany('App\Models\Post','id','post_id');
     }
+
+    public function already_exist_tag()
+    {
+        return Tag::where('tag',$this->tag)->where('post_id',$this->post_id)->exists();
+    }
 }
