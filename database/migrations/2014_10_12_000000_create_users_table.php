619d879216e8f74a82c8cea418dfa2c18d97356c
sirayusan <pikachu.endu@gmail.com>
2021-01-31 17:09:20 +0900

    topページ見た目再修正

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('image')->default('no_image.png');
+            $table->string('image')->default('user_no_image.png');
             $table->string('comment')->nullable();
             $table->rememberToken();
             $table->timestamps();
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -15,21 +15,33 @@ use Illuminate\Support\Facades\Auth;
 </head>
 <body>
     <header>
-        <div class="gloval_fixed_-menu">
-            <a href="{{ route('top.index') }}">photoshare</a>
+        <div class="gloval_fixed_menu">
+            <div class="gloval_fixed_menu_inner">
+                <a href="{{ route('top.index') }}">photoshare</a>
+                <form class="form-box" action="{{ route('tag_search.index') }}" method="get">
+                    <input classclass="form-input" type="text" name="tags">
+                    <button class="form-button"><img src="{{asset('/SystemImage/top_search_icon.png')}}" width="20px" height="20px"></button>
+                </form>
+            </div>
             <input type="checkbox" id="menu-btn-check">
             <label for="menu-btn-check" class="menu-btn"><span></span></label>
             <!--ここからメニュー-->
             <div class="menu-content">
                 <ul>
                     <li>
-                        <a href="{{ route('top.index') }}">メニューリンク1</a>
+                        <a href="{{ route('top.index') }}">topページ</a>
                     </li>
                     <li>
-                        <a href="#">メニューリンク2</a>
+                        @if (Auth::check() === true)
+                        <a href="{{ route('users.show',['user'=>Auth::user()]) }}">プロフィール</a>
+                        @else
+                        <a href="{{ route('login') }}">ログイン</a>
+                        @endif
                     </li>
                     <li>
-                        <a href="#">メニューリンク3</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('login') }}">投稿する</a>
                     </li>
                 </ul>
             </div>
@@ -39,39 +51,41 @@ use Illuminate\Support\Facades\Auth;
     <!-- gloval_fixed_menuの初位置を確保すためのタグ -->
     <div class="wrap"></div>
     <div class="top_image_container">
-        <p id="top_image_bolock"><img id=top_image  src="{{asset('/SystemImage/top.jpg')}}" alt="top画像"></p>
-        <p id="top_title">photoshare</p>
-        <a id="top_register_link" href="{{ route('register') }}">会員登録する</a>
+        <img class="top_image"  src="{{asset('/SystemImage/top.jpg')}}" alt="top画像">
+        <div class="top_image_inner">
+            <p class="top_title">photoshare</p>
+            <a class="top_link ghost_btn" href="{{ route('register') }}">会員登録する</a>
+            <a class="top_link ghost_btn" href="{{ route('posts.create') }}">投稿する</a>
+        </div>
     </div>
-    <form action="{{ route('tag_search.index') }}" method="get">
-        <input type="text" name="tags" value="">
-        <input type="submit" name="" value="検索">
-    </form>
-    <a href="{{ route('posts.create') }}">投稿する</a>
     <br>
-    @if (Auth::check() === true)
-    <a href="{{ route('users.show',['user'=>Auth::user()]) }}">profile</a>
-    @endif
-    <br>
-    <br>
-    <!-- <img src="{{ asset('UserImage/20201203_062307.jpg') }}" alt=""> -->
     <!-- postメソッドで移動させるためにformでpost指定 -->
-    <form method="post" name="form_1" id="form_1" action="{{ route('logout') }}">
-        <input type="hidden" name="user_name" placeholder="ユーザー名">
-        <a href="javascript:form_1.submit()">ログアウト</a>
-        <p>投稿一覧表示</p>
+    <h1 class="line_Darkblue top_index">新着投稿</h1>
+    <div class="posts">
         @foreach ($posts as $post)
         <div class="post">
-            <a href="{{ route('posts.show',['post' => $post->id]) }}">{{ $post['title'] }}
-                <p>タイトル</p>
-                <p>{{ $post['comment'] }}</p>
+            <a href="{{ route('posts.show',['post' => $post->id]) }}">
                 @if ($post->image ==  "no_image.png")
-                <p><img src="{{ asset('/SystemImage/no_image.png') }}" width="80px"></p>
+                <img class="post_img" src="{{ asset('/SystemImage/no_image.png') }}">
                 @else
-                <p><img src="{{ asset("/PostImage/$post->image") }}" width="80px"></p>
+                <img class="post_img" src="{{ asset("/PostImage/$post->image") }}" width="80px">
+                @endif
+                @if (strlen($post->title) <= 36)
+                <p>{{ mb_strimwidth($post->title,0,36) }}</p>
+                @else
+                <p>{{ mb_strimwidth($post->title,0,30) }}.....</p>
                 @endif
             </a>
+            <div class="post_user">
+                @if ($post->user->image ==  "user_no_image.png")
+                <img class="post_icon_Image" src="{{ asset('/SystemImage/'.$post->user->image) }}">
+                @else
+                <img class="post_icon_Image" src="{{ asset('/UserImage/'.$post->user->image) }}">
+                @endif
+                <p>{{ mb_strimwidth($post->user->name,0,28) }}</p>
+            </div>
         </div>
         @endforeach
-    </body>
-    </html>
+    </div>
+</body>
+</html>
