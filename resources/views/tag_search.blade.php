140539bcde8f3c254c5de5754355b40b7703c90f
sirayusan <pikachu.endu@gmail.com>
2021-02-09 18:08:25 +0900

    検索ページ外観調整

--- resources/views/tag_search.blade.php
+++ resources/views/tag_search.blade.php
@@ -1,52 +1,36 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-    <meta charset="utf-8">
-    <title>トップページ</title>
-    <!-- スタイルを明示的にすべてリセットする -->
-    <link href="{{ asset('css/reset.css') }}" rel="stylesheet" />
-    <!-- スタイル指定 -->
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet" />
-</head>
-<body>
-    <header id="sample">
-        <nav id="gloval_fixed_menu">
-            <ul id="gloval_fixed_menu_outer">
-                <ul class="gnav gloval_fixed_menu_inner">
-                    <li>
-                        <a href="">Menu1</a>
-                        <ul>
-                            <li><a href="{{ route('login') }}">会員登録はこちら</a></li>
-                            <li><a href="{{ route('login') }}">会員登録はこちら</a></li>
-                            <li><a href="{{ route('login') }}">会員登録はこちら</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <li class="gloval_fixed_menu_inner"><a href="{{ route('login') }}">会員登録はこちら</a></li>
-                <li class="gloval_fixed_menu_inner"><a href="{{ route('login') }}">会員登録はこちら</a></li>
-                <li class="gloval_fixed_menu_inner"><a href="{{ route('login') }}">会員登録はこちら</a></li>
-            </ul>
-        </nav>
-    </header>
-    <!-- gloval_fixed_menuの初位置を確保すためのタグ -->
-    <div class="wrap"></div>
-    <p>タグ検索結果</p>
-    <a href="{{ route('posts.create') }}">投稿する</a>
-    <br>
-    <a href="{{ route('top.index') }}">トップへ</a>
-    <div>
-        @foreach ($posts as $post)
-          <div class="post">
-            <p>タイトル</p>
-            <a href="{{ route('posts.show',['post' => $post->id]) }}">{{ $post['title'] }}</a>
-            <p>{{ $post['comment'] }}</p>
-            @if ($post->image ==  "no_image.png")
-            <p><img src="{{ asset('/SystemImage/no_image.png') }}" width="80px"></p>
-            @else
-            <p><img src="{{ asset("/PostImage/$post->image") }}" width="80px"></p>
-            @endif
-          </div>
-        @endforeach
-    </div>
-</body>
-</html>
+@extends('layouts/app')
+@section('body')
+<!-- gloval_fixed_menuの初位置を確保すためのタグ -->
+<div class="wrap"></div>
+<main class="posts_search_container">
+    <article class="posts_search_block">
+        <h1 class="line_Darkblue posts_search_index">検索結果</h1>
+        <section class="post_search_posts">
+            @foreach ($posts as $post)
+            <div class="post">
+                <a href="{{ route('posts.show',['post' => $post->id]) }}">
+                    @if ($post->image ==  "no_image.png")
+                    <img class="post_img" src="{{ asset('/SystemImage/no_image.png') }}">
+                    @else
+                    <img class="post_img" src="{{ asset("/PostImage/$post->image") }}" width="80px">
+                    @endif
+                    @if (strlen($post->title) <= 36)
+                    <p>{{ mb_strimwidth($post->title,0,36) }}</p>
+                    @else
+                    <p>{{ mb_strimwidth($post->title,0,30) }}.....</p>
+                    @endif
+                </a>
+                <div class="post_user">
+                    @if ($post->user->image ==  "user_no_image.png")
+                    <img class="post_icon_Image" src="{{ asset('/SystemImage/'.$post->user->image) }}">
+                    @else
+                    <img class="post_icon_Image" src="{{ asset('/UserImage/'.$post->user->image) }}">
+                    @endif
+                    <p>{{ mb_strimwidth($post->user->name,0,28) }}</p>
+                </div>
+            </div>
+            @endforeach
+        </section>
+    </article>
+</main>
+@endsection
