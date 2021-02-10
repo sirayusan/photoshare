e9ee82b4b568af5916a4de40e296a3c7c1f2bd39
sirayusan <pikachu.endu@gmail.com>
2021-02-10 17:43:54 +0900

    フォローの投稿一覧外観調整

--- resources/views/follow_post.blade.php
+++ resources/views/follow_post.blade.php
@@ -1,47 +1,36 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-    <meta charset="utf-8">
-    <title>トップページ</title>
-    <!-- スタイルを明示的にすべてリセットする -->
-    <link href="{{ asset('css/reset.css') }}" rel="stylesheet" />
-    <!-- スタイル指定 -->
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet" />
-</head>
-<body>
-    <header id="sample">
-        <nav id="gloval_fixed_menu">
-            <ul id="gloval_fixed_menu_outer">
-                <ul class="gnav gloval_fixed_menu_inner">
-                    <li>
-                        <a href="">Menu1</a>
-                        <ul>
-                            <li><a href="{{ route('login') }}">会員登録はこちら</a></li>
-                            <li><a href="{{ route('login') }}">会員登録はこちら</a></li>
-                            <li><a href="{{ route('login') }}">会員登録はこちら</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <li class="gloval_fixed_menu_inner"><a href="{{ route('login') }}">会員登録はこちら</a></li>
-                <li class="gloval_fixed_menu_inner"><a href="{{ route('login') }}">会員登録はこちら</a></li>
-                <li class="gloval_fixed_menu_inner"><a href="{{ route('login') }}">会員登録はこちら</a></li>
-            </ul>
-        </nav>
-    </header>
-    <!-- gloval_fixed_menuの初位置を確保すためのタグ -->
-    <div class="wrap"></div>
-    <p>フォローの投稿一覧</p>
-    <a href="{{ route('posts.create') }}">投稿する</a>
-    <br>
-    <a href="{{ route('top.index') }}">トップへ</a>
-    <div>
-        @foreach ($user->follow_user_posts as $post)
+@extends('layouts/app')
+@section('body')
+<!-- gloval_fixed_menuの初位置を確保すためのタグ -->
+<div class="wrap"></div>
+<main class="posts_search_container">
+    <article class="posts_search_block">
+        <h1 class="line_Darkblue posts_search_index">フォローの投稿一覧</h1>
+        <section class="post_search_posts">
+            @foreach ($user->follow_user_posts as $post)
             <div class="post">
-                <p>{{ $post->user->name }}</p>
-                <p><a href="{{ route('comments.index',['post_id' => $post->id]) }}">{{ $post->title }}</a></p>
-                <p>{{ $post->comment }}</p>
+                <a href="{{ route('posts.show',['post' => $post->id]) }}">
+                    @if ($post->image ==  "no_image.png")
+                    <img class="post_img" src="{{ asset('/SystemImage/no_image.png') }}">
+                    @else
+                    <img class="post_img" src="{{ asset("/PostImage/$post->image") }}" width="80px">
+                    @endif
+                    @if (strlen($post->title) <= 36)
+                    <p>{{ mb_strimwidth($post->title,0,36) }}</p>
+                    @else
+                    <p>{{ mb_strimwidth($post->title,0,30) }}.....</p>
+                    @endif
+                </a>
+                <div class="post_user">
+                    @if ($post->user->image ==  "user_no_image.png")
+                    <img class="post_icon_Image" src="{{ asset('/SystemImage/'.$post->user->image) }}">
+                    @else
+                    <img class="post_icon_Image" src="{{ asset('/UserImage/'.$post->user->image) }}">
+                    @endif
+                    <p>{{ mb_strimwidth($post->user->name,0,28) }}</p>
+                </div>
             </div>
-        @endforeach
-    </div>
-</body>
-</html>
+            @endforeach
+        </section>
+    </article>
+</main>
+@endsection
