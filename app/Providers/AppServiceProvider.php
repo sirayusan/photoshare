59af6467e3cece031109fb0b96651ddbd1910327
sirayusan <pikachu.endu@gmail.com>
2021-02-22 16:02:45 +0900

    bytes超過エラー修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if (\App::environment('production')) {
             \URL::forceScheme('https');
+            Schema::defaultStringLength(191);
         }
     }
 }
