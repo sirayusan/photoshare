d8cd812b4f988db398b63a763a1cd82b35b5c8da
sirayusan <pikachu.endu@gmail.com>
2021-03-01 23:32:37 +0900

    ご修正訂正

--- database/seeders/FolllowsTableSeeder.php
+++ database/seeders/FolllowsTableSeeder.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-use App\Models\User;
 
 class FolllowsTableSeeder extends Seeder
 {
