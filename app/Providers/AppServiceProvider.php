ee804d6bfa2f55168f40479183b17289972dbf22
sirayusan <pikachu.endu@gmail.com>
2021-03-02 23:26:36 +0900

    a

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,8 @@ class AppServiceProvider extends ServiceProvider
         if (\App::environment('production')) {
             Schema::defaultStringLength(191);
         }
+        if(config('app.env') === 'production'){
             \URL::forceScheme('https');
+        }
     }
 }
